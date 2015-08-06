4318e219f9b2723d720a342f21fb9d068ab102cf
Rossano <rossanorb@gmail.com>
2015-08-05 23:07:45 -0300

    Configuração do banco configuração dos plugins de permissão

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,13 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+	protected function _initAutoload(){
+            // Add autoloader empty namespace
+            $autoLoader = Zend_Loader_Autoloader::getInstance();
+            $autoLoader->registerNamespace('Aplicacao_');
+            // Return it so that it can be stored by the bootstrap
+            return $autoLoader;
+	}
 
 }
 
