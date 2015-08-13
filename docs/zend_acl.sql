42ba9ec54c4bd4af3aa5390e8e11995fec2fe07f
Rossano <rossanorb@gmail.com>
2015-08-13 12:14:20 -0300

    Criação de trigger para criar herança do usuário padrão

--- docs/zend_acl.sql
+++ docs/zend_acl.sql
@@ -22,7 +22,7 @@ CREATE TABLE `acl` (
   `controller` varchar(100) NOT NULL,
   `action` varchar(100) NOT NULL,
   PRIMARY KEY (`id_acl`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of acl
@@ -34,6 +34,7 @@ INSERT INTO `acl` VALUES ('4', 'noticias', 'adicionar');
 INSERT INTO `acl` VALUES ('5', 'usuarios', 'writer');
 INSERT INTO `acl` VALUES ('6', 'usuarios', 'adicionar');
 INSERT INTO `acl` VALUES ('7', 'auth', 'logout');
+INSERT INTO `acl` VALUES ('9', 'error', 'error');
 
 -- ----------------------------
 -- Table structure for `acl_roles`
@@ -48,7 +49,7 @@ CREATE TABLE `acl_roles` (
   KEY `fk_acl` (`id_acl`),
   CONSTRAINT `fk_acl` FOREIGN KEY (`id_acl`) REFERENCES `acl` (`id_acl`),
   CONSTRAINT `fk_role` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id_role`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of acl_roles
@@ -60,6 +61,7 @@ INSERT INTO `acl_roles` VALUES ('4', '2', '4');
 INSERT INTO `acl_roles` VALUES ('5', '3', '5');
 INSERT INTO `acl_roles` VALUES ('6', '3', '6');
 INSERT INTO `acl_roles` VALUES ('7', '2', '7');
+INSERT INTO `acl_roles` VALUES ('10', '1', '9');
 
 -- ----------------------------
 -- Table structure for `inherit`
@@ -69,14 +71,16 @@ CREATE TABLE `inherit` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_role` int(10) unsigned NOT NULL,
   `id_role_rel` int(10) unsigned NOT NULL,
+  `role` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of inherit
 -- ----------------------------
-INSERT INTO `inherit` VALUES ('1', '2', '1');
-INSERT INTO `inherit` VALUES ('2', '3', '2');
+INSERT INTO `inherit` VALUES ('9', '2', '1', 'guest');
+INSERT INTO `inherit` VALUES ('10', '3', '2', 'writer');
+INSERT INTO `inherit` VALUES ('11', '3', '1', 'guest');
 
 -- ----------------------------
 -- Table structure for `roles`
@@ -85,7 +89,7 @@ DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
   `id_role` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `role` varchar(20) NOT NULL,
-  `role_default` bit(1) NOT NULL DEFAULT b'1',
+  `role_default` bit(1) NOT NULL,
   PRIMARY KEY (`id_role`),
   UNIQUE KEY `idx_role` (`role`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
@@ -115,3 +119,25 @@ CREATE TABLE `users` (
 -- ----------------------------
 INSERT INTO `users` VALUES ('1', '2', 'rossano', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'Rossano');
 INSERT INTO `users` VALUES ('2', '3', 'root', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 'root');
+DROP TRIGGER IF EXISTS `tr_cria_heranca_default`;
+DELIMITER ;;
+CREATE TRIGGER `tr_cria_heranca_default` AFTER INSERT ON `roles` FOR EACH ROW BEGIN
+	DECLARE column1 varchar(20);
+    DECLARE column2 varchar(20);
+    
+    
+	select id_role, role  INTO column1, column2  FROM roles WHERE role_default = 1;
+        
+        
+    Insert into inherit (id_role, id_role_rel, role) VALUES (NEW.id_role, column1, column2 ) ;
+END
+;;
+DELIMITER ;
+DROP TRIGGER IF EXISTS `tr_delete_heranca_default`;
+DELIMITER ;;
+CREATE TRIGGER `tr_delete_heranca_default` AFTER DELETE ON `roles` FOR EACH ROW BEGIN
+	
+	DELETE FROM inherit WHERE id_role = old.id_role;
+END
+;;
+DELIMITER ;
